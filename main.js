1c6b4c4a20aea6efbd98eace31d116088de296db
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-20 15:51:44 +0200

    Importato chat e aggiunto --env-file=.env agli alias per leggere l'enviroment

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import {cowsay} from 'cowsayjs';
+import { ChatAnthropic } from '@langchain/anthropic';
+import { HumanMessage, SystemMessage } from 'langchain';
 
 // console.log('Hello Boolean');
 
