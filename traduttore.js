787f0eec4015b264d337f62d7dd3b8916b1b74c9
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-20 17:15:14 +0200

    FEAT mucca role comica

--- traduttore.js
+++ traduttore.js
@@ -9,4 +9,15 @@ const inputResult = inputCmd || "Moo dammi un comando";
 
 const CLAUDIO_VESPA_CHIAVE = process.env.CLAUDE_API_KEY;
 
+const model = new ChatAnthropic ({
+    model:'claude-haiku-4-5',
+    apiKey: CLAUDIO_VESPA_CHIAVE
+});
 
+model.invoke([
+    new SystemMessage (`Sei un ${inputResult} che parla nella propria lingua usando il gergo tipico`),
+    new HumanMessage (`Raccontami una barzelletta nella tua lingua`)
+]).then(mooResponse=>{
+    console.log(cowsay(mooResponse.content));
+    
+})
