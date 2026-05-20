93ab953fdcec510c6905064fa6f638b9e2641a3c
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-20 16:53:20 +0200

    Setup input da cmd

--- traduttore.js
+++ traduttore.js
@@ -2,3 +2,9 @@ import {cowsay} from 'cowsayjs';
 import { ChatAnthropic } from '@langchain/anthropic';
 import { HumanMessage, SystemMessage } from 'langchain';
 
+const inputCmd = process.argv.slice(2).join(' ');
+
+// Se input vuoto, restituisce Moooo
+const inputResult = inputCmd || "Moo dammi un comando";
+
+console.log(inputResult);
