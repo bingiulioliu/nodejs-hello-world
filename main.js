f3accbf8db81eca6a01b9973f5d1a0cca6aa414c
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-20 15:56:47 +0200

    Settata claude api key e modello

--- main.js
+++ main.js
@@ -6,4 +6,15 @@ import { HumanMessage, SystemMessage } from 'langchain';
 
 const frase = process.argv.slice(2).join(' ');
 
+const parametriCmd = process.argv;
+
+const variaviliEnv = process.env;
+
+const CLAUDE_API_KEY = variaviliEnv.CLAUDE_API_KEY;
+
+const model = new ChatAnthropic({
+    model:'claude-haiku-4-5',
+    apiKey: CLAUDE_API_KEY
+})
+
 console.log(cowsay(frase));
