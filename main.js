eab63f7559216a946191e300662467647165001d
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-20 16:15:13 +0200

    FEAT aggiunta mucca comica da anthropic

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ const variaviliEnv = process.env;
 
 const CLAUDE_API_KEY = variaviliEnv.CLAUDE_API_KEY;
 
+console.log(process.env.ANTHROPIC_API_KEY);
+
+
 const model = new ChatAnthropic({
     model:'claude-haiku-4-5',
     apiKey: CLAUDE_API_KEY
+});
+
+model.invoke([
+    new SystemMessage ('Sei una mucca che fa il comico, nello specifico sul black humor. Inizi e concludi le frasi muggendo'),
+    new HumanMessage ('Mi racconti una barzelletta sulle mucche?')
+]).then(mooResponse =>{
+    console.log(cowsay(mooResponse.content));
 })
 
-console.log(cowsay(frase));
